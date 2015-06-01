53882d4f76941e28d41e8857d238a14066b053c9
divac lin <divac.d.zoro@gmail.com>
2015-06-01 14:37:11 +0800

    string

--- js/key.js
+++ js/key.js
@@ -1,2 +1,2 @@
 //Parse.initialize
-Parse.initialize(bAMceHECEizL9iWqat9IJqgLRBmta8WmhrivYOIL,4Fyg7B8e4Foovs4XOVpnKZnvb9LJMNstYgp9tJe1);
+Parse.initialize("bAMceHECEizL9iWqat9IJqgLRBmta8WmhrivYOIL","4Fyg7B8e4Foovs4XOVpnKZnvb9LJMNstYgp9tJe1");
